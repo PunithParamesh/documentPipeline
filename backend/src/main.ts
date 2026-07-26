8a87500152b11aecdd75e8a028ffa7b7b9561d7e
PunithParamesh <1ms20ec070@gmail.com>
2026-07-26 23:18:09 +0530

    Update nest app to listen on 0.0.0.0 for Docker support

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -24,7 +24,8 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
-  Logger.log(`Application is running on: http://localhost:${port}`);
+
+  await app.listen(port, '0.0.0.0');
+  Logger.log(`Application is running on port ${port}`);
 }
 bootstrap();
